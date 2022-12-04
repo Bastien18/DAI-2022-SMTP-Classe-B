4402c7d5a26ad666fdbaae2f9318bc9261fa9427
Pillonel-on-github <bastien.pil01@gmail.com>
2022-12-04 12:51:09 +0100

    Reorganisation of the software structure

--- SRC/SMTP_Prank_Brasey_Pillonel/src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        SMTPManager test = new SMTPManager("127.0.0.1",25000);
-        test.sendMail("from","to","test", "content");
-    }
-}
