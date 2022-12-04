d4182b08a48ac2bc2bebe27e699d9a7b1f933cdf
Pillonel-on-github <bastien.pil01@gmail.com>
2022-12-04 12:55:12 +0100

    Main implementation

---
+++ SRC/SMTP_Prank_Brasey_Pillonel/src/main/java/MailRobot.java
@@ -0,0 +1,37 @@
+import FileConfigurer.IFileConfigurer;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import FileConfigurer.*;
+import SMTP.SMTPClient;
+import bot.prank.Prank;
+import bot.prank.PrankGenerator;
+import java.util.concurrent.TimeUnit;
+
+public class MailRobot {
+
+    private static final Logger LOG = Logger.getLogger(MailRobot.class.getName());
+    public static void main(String[] args) {
+        IFileConfigurer fc;
+        try{
+            fc = new FileConfigurer();
+        }catch(Exception ex){
+            LOG.log(Level.SEVERE, ex.toString(), ex);
+            return;
+        }
+
+        PrankGenerator generator = new PrankGenerator(fc);
+        List<Prank> pranks = generator.generatePrank();
+        SMTPClient client = new SMTPClient(fc.getSmtpServerAddress(), fc.getSmtpServerPort());
+
+        try{
+            for(Prank prank : pranks){
+                client.sendMail(prank.generateEmail());
+                TimeUnit.MILLISECONDS.sleep(500);
+            }
+        }catch(Exception ex){
+            LOG.log(Level.SEVERE, ex.toString(), ex);
+        }
+    }
+}
